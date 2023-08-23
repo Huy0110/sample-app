8f705d82b8fba3f5ebc00fd18686d616db808ca3
Huy0110 <duchuy01102001@gmail.com>
2023-08-23 16:20:38 +0700

    Finish chapter 7: Tao moi user va xem thong tin chi tiet user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,13 @@
 class User < ApplicationRecord
   before_save{self.email.downcase!}
-  validates :name, presence: true, length:
-  {
-    maximum: Settings.validate.name.length.max
-  }
-  validates :email, presence: true, length:
-  {
-    maximum: Settings.validate.email.length.max
-  },
-                    format: {with: Settings.validate.VALID_EMAIL_REGEX},
-                    uniqueness: true
+
+  validates :name, presence: true,
+    length: {maximum: Settings.validate.digits.length_50}
+  validates :email, presence: true,
+    length: {maximum: Settings.validate.digits.length_255},
+    format: {with: Settings.validate.email_regex},
+    uniqueness: true
+  validates :password, presence: true, length: {minimum: Settings.validate.digits.length_6}
+
   has_secure_password
-  validates :password, presence: true, length:
-  {
-    minimum: Settings.validate.password.length.min
-  }
 end
